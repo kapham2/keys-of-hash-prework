43dfcf2c0045fad32423ba1b0a7a9f614adf1119
Kim Pham <pham.kim.a@gmail.com>
2018-08-24 22:51:38 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,13 @@
 class Hash
   def keys_of(*arguments)
+    array = []
+    
     arguments.each do |arg|
-      if arg == self
+      self.each do |key, value|
+        if arg == value
+          array.push(key)
+        end
+      end
+    end
   end
 end
